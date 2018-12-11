8c16b064a3e02aaaea5e1e6d25e78fc67bbcc78d
Vasya Kacheshmarova <vasq1989@gmail.com>
2018-12-11 08:46:53 +0200

    jQuery 3.3.1 compatibility

--- src/js/modules/infragistics.ui.editors.js
+++ src/js/modules/infragistics.ui.editors.js
@@ -2336,7 +2336,7 @@
 			}
 		},
 		_calculateDropDownListOrientation: function () {
-			var containerOffset = this._editorContainer.offset(),
+			var containerOffset = this._editorContainer.igOffset(),
 				containerTop = containerOffset.top,
 				containerHeight = parseFloat(this._editorContainer.css("height")),
 				dropDownAndEditorHeight = parseInt(containerTop + containerHeight + this._listInitialHeight),
@@ -2356,7 +2356,7 @@
 			return orientation;
 		},
 		_positionDropDownList: function () {
-			var containerOffset = this._editorContainer.offset(),
+			var containerOffset = this._editorContainer.igOffset(),
 			containerTop = containerOffset.top,
 			containerLeft = containerOffset.left,
 			containerHeight = parseFloat(this._editorContainer.css("height")),
@@ -3269,8 +3269,8 @@
 			return result;
 		},
 		_elementPositionInViewport: function (el) {
-				var areaTop = Math.ceil(el.parent().offset().top),
-					elementoffset = Math.ceil(el.offset().top),
+				var areaTop = Math.ceil(el.parent().igOffset().top),
+					elementoffset = Math.ceil(el.igOffset().top),
 					elementHeight = Math.ceil(el.outerHeight()),
 					listVisibleHeight = el.parent().outerHeight(), result;
 				if (elementoffset - areaTop < 0) {
@@ -3295,7 +3295,7 @@
 						newItem.outerHeight());
 				} else if (position === "bottom") {
 					this._dropDownList.scrollTop(this._dropDownList.scrollTop() +
-						newItem.position().top);
+						newItem.igPosition().top);
 				}
 				currentItem.removeClass(this.css.listItemActive,
 					this.options.listItemHoverDuration);
@@ -3317,7 +3317,7 @@
 						newItem.outerHeight());
 				} else if (position === "top") {
 					this._dropDownList.scrollTop(this._dropDownList.scrollTop() +
-						newItem.position().top);
+						newItem.igPosition().top);
 				}
 				currentItem.removeClass(this.css.listItemActive,
 					this.options.listItemHoverDuration);
@@ -3431,7 +3431,7 @@
 			}
 			if (this._elementPositionInViewport(activeItem) !== "inside") {
 				this._dropDownList.scrollTop(this._dropDownList.scrollTop() +
-						activeItem.position().top);
+						activeItem.igPosition().top);
 			}
 			activeItem.attr("data-active", true);
 		},
@@ -3846,7 +3846,7 @@
 					position = this._elementPositionInViewport(newSelectedItem);
 					if (position !== "inside") {
 						this._dropDownList.scrollTop(this._dropDownList.scrollTop() +
-							newSelectedItem.position().top);
+							newSelectedItem.igPosition().top);
 					}
 					this._clearDropDownHoverActiveItem();
 					newSelectedItem.attr("data-active", true);
